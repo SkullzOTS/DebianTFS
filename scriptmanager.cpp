48be07d6d7fe29c973fa7beb16377528a1f17c68
SkullzOTS <skullz.otserv@gmail.com>
2023-07-28 10:50:21 -0300

    Fix_Compilation

--- scriptmanager.cpp
+++ scriptmanager.cpp
@@ -119,7 +119,8 @@ bool ScriptingManager::loadMods()
 	bool enabled = false;
 	for(boost::filesystem::directory_iterator it(modsPath), end; it != end; ++it)
 	{
-		std::string s = it->leaf();
+		// std::string s = it->leaf();  //COMPILAÇÃO EM WINDOWS
+		std::string s = it->path().filename().string();  //COMPILAÇÃO EM LINUX
 		if(boost::filesystem::is_directory(it->status()) && (s.size() > 4 ? s.substr(s.size() - 4) : "") != ".xml")
 			continue;
 
